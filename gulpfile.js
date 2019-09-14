0cfc219f6869d37b50c7f3eea8572cd96e1f6165
Adam Gonzales <adamgonzales1@gmail.com>
2019-09-14 16:58:08 -0700

    get gulp file working

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ const { src, dest, watch, series, parallel } = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync');
 
 const files = {
+  imgPath: 'app/images/*.*',
+  htmlPath: './index.html',
   scssPath: 'app/scss/**/*.scss',
-  jsPath: 'app/js/**/*.js'
+  jsPath: 'app/js/**/*.js',
+
 }
 
 function scssTask() {
@@ -16,23 +20,25 @@ function scssTask() {
   .pipe(sass())
   // .pipe(autoprefixer())
   .pipe(sourcemaps.write('.'))
-  .pipe(dest('dist')
-  );
+  .pipe(dest('dist'))
+  .pipe(browserSync.stream());
 }
 
 function jsTask() {
   return src([files.jsPath])
   .pipe(concat('index.js'))
-  // .pipe(uglify())
-  .pipe(dest('dist')
-  );
+  .pipe(uglify())
+  .pipe(dest('dist'));
 }
 
 function watchTask() {
-  watch(
-    [files.scssPath, files.jsPath],
-    parallel(scssTask, jsTask)
-  );
+  browserSync.init({
+    server: "./"
+  });
+  watch(files.scssPath, scssTask);
+  watch(files.imgPath).on('change', browserSync.reload);
+  watch(files.htmlPath).on('change', browserSync.reload);
+  watch(files.jsPath, jsTask).on('change', browserSync.reload);
 }
 
 exports.default = series(
